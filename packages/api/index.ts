d6ebfb676c698dc2470c56e9714cc8d824ac19fb
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2025-03-17 21:16:23 +0100

    search is case insensitive

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,16 +1,16 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { MongoClient } from 'mongodb';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { MongoClient } from "mongodb";
 
 dotenv.config();
 
-if (process.env.NODE_ENV !== 'production' && !process.env.DATABASE_URL) {
-  await import('./db/startAndSeedMemoryDB');
+if (process.env.NODE_ENV !== "production" && !process.env.DATABASE_URL) {
+  await import("./db/startAndSeedMemoryDB");
 }
 
 const PORT = process.env.PORT || 3001;
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
@@ -18,63 +18,62 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/search', async (req: Request, res: Response) => {
+app.get("/search", async (req: Request, res: Response) => {
   const mongoClient = new MongoClient(DATABASE_URL);
-  console.log('Connecting to MongoDB...');
+  console.log("Connecting to MongoDB...");
   const { searchTerm, limit, page } = req.query;
   try {
     await mongoClient.connect();
-    console.log('Successfully connected to MongoDB!');
+    console.log("Successfully connected to MongoDB!");
     const db = mongoClient.db();
 
-    const hotelCollection = db.collection('hotels');
-    const citiesCollection = db.collection('cities');
-    const countriesCollection = db.collection('countries');
+    const hotelCollection = db.collection("hotels");
+    const citiesCollection = db.collection("cities");
+    const countriesCollection = db.collection("countries");
 
     console.log(
-      'Searching for hotels, cities and countries with matching names'
+      "Searching for hotels, cities and countries with matching names"
     );
+
+    const regEx = RegExp(`${searchTerm}`, "i");
 
     const [hotels, cities, countries] = await Promise.all([
       hotelCollection
         .find({
-          $or: [
-            { hotel_name: { $regex: `.*${searchTerm}.*` } },
-            { chain_name: { $regex: `.*${searchTerm}.*` } },
-          ],
+          $or: [{ hotel_name: regEx }, { chain_name: regEx }],
         })
         .toArray(),
       citiesCollection
         .aggregate([
-          { $match: { name: { $regex: `.*${searchTerm}.*` } } },
+          { $match: { name: regEx } },
           {
             $lookup: {
-              from: 'hotels',
-              localField: 'city',
-              foreignField: 'name',
-              as: 'hotels',
+              from: "hotels",
+              localField: "city",
+              foreignField: "name",
+              as: "hotels",
             },
           },
         ])
         .toArray(),
       countriesCollection
         .aggregate([
-          { $match: { country: { $regex: `.*${searchTerm}.*` } } },
+          { $match: { country: regEx } },
           {
             $lookup: {
-              from: 'hotels',
-              localField: 'country',
-              foreignField: 'country',
-              as: 'hotels',
+              from: "hotels",
+              localField: "country",
+              foreignField: "country",
+              as: "hotels",
             },
           },
         ])
         .toArray(),
     ]);
 
-    console.log('Successfully found matching resources');
+    console.log("Successfully found matching resources");
 
-    console.log('Mapping matching country and city hotels into hotels array');
+    console.log("Mapping matching country and city hotels into hotels array");
     const allHotels = [cities, countries].reduce(
       (acc, next) => {
         next.forEach((item) => {
@@ -88,10 +87,10 @@ app.get('/search', async (req: Request, res: Response) => {
     );
 
     console.log(
-      'Successfully mapped hotels from matching countries and cities'
+      "Successfully mapped hotels from matching countries and cities"
     );
 
-    console.log('Filtering out duplicate hotels');
+    console.log("Filtering out duplicate hotels");
 
     const uniqHotelLookup: { [key: string]: boolean } = {};
 
@@ -103,9 +102,9 @@ app.get('/search', async (req: Request, res: Response) => {
       return true;
     });
 
-    console.log('Successfully filtered out duplicate hotels');
+    console.log("Successfully filtered out duplicate hotels");
 
-    console.log('Returning payload');
+    console.log("Returning payload");
 
     res.send({
       hotels: uniqHotels,
@@ -113,7 +112,7 @@ app.get('/search', async (req: Request, res: Response) => {
       countries,
     });
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   } finally {
     await mongoClient.close();
   }
